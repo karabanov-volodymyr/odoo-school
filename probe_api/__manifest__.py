a9f0ff5729fd95126fe28ea6e58e7719c6105895
vovik <vk@kitworks.systems>
2023-05-16 13:34:29 +0300

    api probe

---
+++ probe_api/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    'name': 'API probe',
+
+    'category': 'Customizations',
+    'license': 'OPL-1',
+    'version': '15.0.1.0.0',
+
+    'depends': [
+    ],
+
+    'external_dependencies': {'python': [], },
+
+    'data': [
+        'security/ir.model.access.csv',
+
+    ],
+    'demo': [
+    ],
+
+    'installable': True,
+    'auto_install': False,
+
+}
